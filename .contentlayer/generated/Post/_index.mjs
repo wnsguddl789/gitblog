cce28d3f2399ab7ddf98388d258c85195b11fa17
bizbaeja <75407823+bizbaeja@users.noreply.github.com>
2022-11-09 20:19:45 +0900

    sixth

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -4,6 +4,7 @@ import fifthMdx from './fifth.mdx.json' assert { type: 'json' }
 import firstMdx from './first.mdx.json' assert { type: 'json' }
 import fourthMdx from './fourth.mdx.json' assert { type: 'json' }
 import secondMdx from './second.mdx.json' assert { type: 'json' }
+import sixthMdx from './sixth.mdx.json' assert { type: 'json' }
 import thirdMdx from './third.mdx.json' assert { type: 'json' }
 
-export const allPosts = [fifthMdx, firstMdx, fourthMdx, secondMdx, thirdMdx]
+export const allPosts = [fifthMdx, firstMdx, fourthMdx, secondMdx, sixthMdx, thirdMdx]

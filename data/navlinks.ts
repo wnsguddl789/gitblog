0d4cbd66a5821514905940725865070ca09ee11a
aeong <aeong@aeongui-MacBookPro.local>
2022-12-18 00:01:31 +0900

    state management

--- data/navlinks.ts
+++ data/navlinks.ts
@@ -1,6 +1,7 @@
 const navlinks: { title: string; link: string }[] = [
   { title: 'Home', link: '/' },
-  { title: 'Blog', link: '/blog' }
+  { title: 'Blog', link: '/blog' },
+  { title: 'TIL', link: '/til' }
 ];
 
 export default navlinks;

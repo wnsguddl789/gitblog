01dbca4b01758c4e0918f9940f94e2813d0da19e
aeong <aeong@aeongui-MacBookPro.local>
2022-12-23 10:57:41 +0900

    백틱 빼지 말기

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -14,6 +14,8 @@ import _30thMdx from './_30th.mdx.json' assert { type: 'json' }
 import _31thMdx from './_31th.mdx.json' assert { type: 'json' }
 import _32thMdx from './_32th.mdx.json' assert { type: 'json' }
 import _33thMdx from './_33th.mdx.json' assert { type: 'json' }
+import _34thMdx from './_34th.mdx.json' assert { type: 'json' }
+import _35thMdx from './_35th.mdx.json' assert { type: 'json' }
 import eighteenthMdx from './eighteenth.mdx.json' assert { type: 'json' }
 import eighthMdx from './eighth.mdx.json' assert { type: 'json' }
 import eleventhMdx from './eleventh.mdx.json' assert { type: 'json' }
@@ -33,4 +35,4 @@ import thirdMdx from './third.mdx.json' assert { type: 'json' }
 import thirteenthMdx from './thirteenth.mdx.json' assert { type: 'json' }
 import twelvfthMdx from './twelvfth.mdx.json' assert { type: 'json' }
 
-export const allPosts = [_20thMdx, _21thMdx, _22thMdx, _23thMdx, _24thMdx, _25thMdx, _26thMdx, _27thMdx, _28thMdx, _29thMdx, _30thMdx, _31thMdx, _32thMdx, _33thMdx, eighteenthMdx, eighthMdx, eleventhMdx, fifteenthMdx, fifthMdx, firstMdx, fourteenthMdx, fourthMdx, ninthMdx, secondMdx, seventeenthMdx, seventhMdx, sixteenthMdx, sixthMdx, tenthMdx, thirdMdx, thirteenthMdx, twelvfthMdx]
+export const allPosts = [_20thMdx, _21thMdx, _22thMdx, _23thMdx, _24thMdx, _25thMdx, _26thMdx, _27thMdx, _28thMdx, _29thMdx, _30thMdx, _31thMdx, _32thMdx, _33thMdx, _34thMdx, _35thMdx, eighteenthMdx, eighthMdx, eleventhMdx, fifteenthMdx, fifthMdx, firstMdx, fourteenthMdx, fourthMdx, ninthMdx, secondMdx, seventeenthMdx, seventhMdx, sixteenthMdx, sixthMdx, tenthMdx, thirdMdx, thirteenthMdx, twelvfthMdx]

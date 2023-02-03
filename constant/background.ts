9b7a2dfc07d2fc85b2376b303b6f49578c68eb55
bbakjun <wnsguddl789@gmail.com>
2023-02-03 12:22:02 +0900

    fix : 필요없는 파일 제거

---
+++ constant/background.ts
@@ -0,0 +1,27 @@
+export const images = [
+  '1.jpg',
+  '2.jpg',
+  '3.jpg',
+  '4.jpg',
+  '5.jpg',
+  '6.jpeg',
+  '7.jpeg',
+  '8.jpeg',
+  '9.jpeg',
+  '10.jpeg',
+  '11.jpeg',
+  '12.jpeg',
+  '13.jpeg',
+  '14.jpeg',
+  '15.jpeg',
+  '16.jpeg',
+  '17.jpeg',
+  '18.jpeg',
+  '19.jpeg',
+  '20.jpeg',
+  '21.jpeg'
+];
+
+export const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+`images/${chosenImage}`;
--- data/metadata.ts
+++ data/metadata.ts
@@ -1,10 +1,11 @@
-import { todaysQoute } from './../pages/quote';
+import { todaysQuote } from '../constant/quote';
+
 const metadata = {
   title: '땃쥐의 개발블로그',
-  quote: `${todaysQoute.quote}`,
-  authors: `${todaysQoute.author}`,
+  quote: `${todaysQuote.quote}`,
+  authors: `${todaysQuote.author}`,
   description: "who's blog",
   author: 'Bizbaeja'
-};
+} as const;
 
 export default metadata;
